d28d85ea84abc5f970bef2192b29d11e4e7c7aad
SANGROK KANG <sr.kang@lge.com>
2021-02-16 18:55:37 +0900

    Modified files

--- gatsby-node.js
+++ gatsby-node.js
@@ -59,7 +59,9 @@ exports.createPages = ({ graphql, actions }) => {
 
         // Create Page pages.
         const pageTemplate = path.resolve("./src/templates/page.js")
-        const portfolioUnderContentTemplate = path.resolve("./src/templates/portfolioUnderContent.js")
+        const portfolioUnderContentTemplate = path.resolve(
+          "./src/templates/portfolioUnderContent.js"
+        )
         // We want to create a detailed page for each
         // page node. We'll just use the WordPress Slug for the slug.
         // The Page ID is prefixed with 'PAGE_'
@@ -74,14 +76,18 @@ exports.createPages = ({ graphql, actions }) => {
             // optional but is often necessary so the template
             // can query data specific to each page.
             path: `/${edge.node.slug}/`,
-            component: slash(edge.node.template === 'portfolio_under_content.php' ? portfolioUnderContentTemplate : pageTemplate),
+            component: slash(
+              edge.node.template === "portfolio_under_content.php"
+                ? portfolioUnderContentTemplate
+                : pageTemplate
+            ),
             context: edge.node,
           })
         })
       })
       // ==== END PAGES ====
 
-      // ==== POSTS (WORDPRESS NATIVE AND ACF) ====
+      // ==== PORTFOLIO (WORDPRESS NATIVE AND ACF) ====
       .then(() => {
         graphql(
           `
@@ -96,6 +102,9 @@ exports.createPages = ({ graphql, actions }) => {
                     content
                     featured_media {
                       source_url
+                    }
+                    acf {
+                      portfolio_url
                     }
                   }
                 }
@@ -118,9 +127,64 @@ exports.createPages = ({ graphql, actions }) => {
               context: edge.node,
             })
           })
-          resolve()
         })
       })
-    // ==== END POSTS ====
+      // ==== END FORTFOLIO ====
+      // ==== BLOG POST ====
+      .then(() => {
+        graphql(`
+          {
+            allWordpressPost {
+              edges {
+                node {
+                  excerpt
+                  wordpress_id
+                  date(formatString: "Do MMM YYYY HH:mm")
+                  title
+                  content
+                  slug
+                }
+              }
+            }
+          }
+        `).then(result => {
+          if (result.errors) {
+            console.log(result.errors)
+            reject(result.errors)
+          }
+
+          const posts = result.data.allWordpressPost.edges
+          const postPerPage = 2
+          const numberOfPages = Math.ceil(posts.length / postPerPage)
+          const blogPostListTemplate = path.resolve('./src/templates/blogPostList.js')
+
+          Array.from({ length: numberOfPages }).forEach((page, index) => {
+            createPage({
+              component: slash(blogPostListTemplate),
+              path: index === 0 ? '/blog' : `/blog/${index + 1}`,
+              context: {
+                posts: posts.slice(
+                  index * postPerPage,
+                  (index * postPerPage) + postPerPage
+                ),
+                numberOfPages,
+                currentPage: index + 1
+              }
+            })
+          })
+
+          const pageTemplate = path.resolve("./src/templates/page.js")
+          _.each(posts, (post) => {
+            createPage({
+              path: `/post/${post.node.slug}`,
+              component: slash(pageTemplate),
+              context: post.node
+            })
+          })
+
+          resolve()
+
+        })
+      })
   })
 }
